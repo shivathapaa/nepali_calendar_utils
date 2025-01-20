ed7cac46b59393327a5324aa440957d9f57145c8
Shiva Thapa <shivathapa0600@gmail.com>
2025-01-21 01:02:16 +0545

    [Fix] Return type of the extenstion function

--- src/nepali_calendar/data/custom_calendar.py
+++ src/nepali_calendar/data/custom_calendar.py
@@ -29,47 +29,6 @@ class SimpleTime:
     second: int
     nanosecond: int
 
-
-@dataclass(frozen=True)
-class CustomCalendar:
-    """
-    Represents a date in a custom calendar system with detailed information.
-    """
-    year: int
-    month: int
-    day_of_month: int
-    era: int  # 1 for AD, 2 for BS
-    first_day_of_month: int
-    last_day_of_month: int
-    total_days_in_month: int
-    day_of_week_in_month: int = -1
-    day_of_week: int = -1
-    day_of_year: int = -1
-    week_of_month: int = -1
-    week_of_year: int = -1
-
-    def to_simple_date(self) -> "SimpleDate":
-        """
-        Converts this CustomCalendar object to a SimpleDate object.
-        """
-        return SimpleDate(
-            year=self.year, month=self.month, day_of_month=self.day_of_month
-        )
-
-    def to_nepali_month_calendar(self) -> "NepaliMonthCalendar":
-        """
-        Converts this CustomCalendar object to a NepaliMonthCalendar object.
-        """
-        return NepaliMonthCalendar(
-            year=self.year,
-            month=self.month,
-            total_days_in_month=self.total_days_in_month,
-            first_day_of_month=self.first_day_of_month,
-            last_day_of_month=self.last_day_of_month,
-            # days_from_start_of_week_to_first_of_month=self.first_day_of_month - 1,
-        )
-
-
 @dataclass(frozen=True)
 class NepaliMonthCalendar:
     """
@@ -90,3 +49,43 @@ class NepaliMonthCalendar:
         Returns the position of a NepaliMonthCalendar within a given years range.
         """
         return (self.year - years.start) * 12 + self.month - 1
+
+
+@dataclass(frozen=True)
+class CustomCalendar:
+    """
+    Represents a date in a custom calendar system with detailed information.
+    """
+    year: int
+    month: int
+    day_of_month: int
+    era: int  # 1 for AD, 2 for BS
+    first_day_of_month: int
+    last_day_of_month: int
+    total_days_in_month: int
+    day_of_week_in_month: int = -1
+    day_of_week: int = -1
+    day_of_year: int = -1
+    week_of_month: int = -1
+    week_of_year: int = -1
+
+    def to_simple_date(self) -> SimpleDate:
+        """
+        Converts this CustomCalendar object to a SimpleDate object.
+        """
+        return SimpleDate(
+            year=self.year, month=self.month, day_of_month=self.day_of_month
+        )
+
+    def to_nepali_month_calendar(self) -> NepaliMonthCalendar:
+        """
+        Converts this CustomCalendar object to a NepaliMonthCalendar object.
+        """
+        return NepaliMonthCalendar(
+            year=self.year,
+            month=self.month,
+            total_days_in_month=self.total_days_in_month,
+            first_day_of_month=self.first_day_of_month,
+            last_day_of_month=self.last_day_of_month,
+            # days_from_start_of_week_to_first_of_month=self.first_day_of_month - 1,
+        )
